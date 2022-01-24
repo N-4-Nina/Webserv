8affa2926edb2c798f169e7fbd855ec6de2fb509
Mel-louie <mdesfont@student.42.fr>
2022-01-24 19:15:00 +0100

    what a lovely raccoon on our website :)

---
+++ testsCGI/cgi-bin/index.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int	main( void ) {
+
+	std::cout << "Content-Type: text/html\n\n";
+	std::cout << "<!DOCTYPE>\n<html>\n";
+	std::cout << "\t\t<title>My super duper website!</title>\n";
+	std::cout << "\t<body>\n";
+
+	std::cout << "\t\t<h1>:0</h1>\n";
+	std::cout << "\t\t\t<p>it's working, i am a c++</p>\n";
+
+	std::cout << "\t</body>\n";
+	return (0);
+}
