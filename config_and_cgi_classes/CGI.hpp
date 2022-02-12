7ce443caf8fd15fac364a6f90442a8994e2bd7ab
Mel-louie <mdesfont@student.42.fr>
2022-02-12 18:31:07 +0100

    update

--- config_and_cgi_classes/CGI.hpp
+++ config_and_cgi_classes/CGI.hpp
@@ -1,3 +1,13 @@
+// when method == (POST || GET)
+// set binary (path)
+// execute
+//      built env (meta-variables https://datatracker.ietf.org/doc/html/rfc3875#section-4.1) passed to execve
+//      build execve 2nd arguments (array of string with path to binary (_binary) and arguments (target))
+//      Pipe / Fork / Excve / Dup2 to write output to file and make CGI read the POST Body
+//      in the parent, write BODY in STDIN of child with tube 
+//      then, parse the CGI output, knowing that its output format will always be (whatever the CGI is) : headers ... \r\n\r\n ... body
+// get output
+
 #ifndef CGI_HPP
 # define CGI_HPP
 
@@ -11,6 +21,8 @@ class CGI
 
         void set_binary(str_t path);
         void set_input(str_t content); //  in a CGI-BIN environment, you read the data from STDIN
+
+        void exec_cgi(str_t target);
 
     private:
         str_t _binary;
